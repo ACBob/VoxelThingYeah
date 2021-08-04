f4ee4d98163ea19b46479d5d9667ef10f3d04e74
ACBob <acbobcat@protonmail.com>
2021-08-04 13:31:46 +0100

    Nearest Neighbour

--- src/sdlbgfx/bgfxtexturemanager.cpp
+++ src/sdlbgfx/bgfxtexturemanager.cpp
@@ -25,7 +25,8 @@ Texture TextureManager::LoadTexture(const char* path)
 	const bgfx::Memory* mem = bgfx::alloc(image.size());
 	memcpy(mem->data, image.data(), image.size());
 
-	bgfx::TextureHandle tex = bgfx::createTexture2D(width, height, false, 1, bgfx::TextureFormat::Enum::RGBA8, 0, mem);
+	// TODO: I *think* clamp keeps the texture from sampling outside its' UV, I'm not sure
+	bgfx::TextureHandle tex = bgfx::createTexture2D(width, height, false, 1, bgfx::TextureFormat::Enum::RGBA8, BGFX_SAMPLER_POINT | BGFX_SAMPLER_U_CLAMP, mem);
 
 	loadedTextures.push_back(reinterpret_cast<Texture>(tex.idx));
 
