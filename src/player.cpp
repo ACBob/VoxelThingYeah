4d3b7edcc1ab565c8393aefabddf33ae102c88a8
ACBob <acbobcat@protonmail.com>
2021-08-05 00:17:37 +0100

    Add left/right, increase sensitivity

--- src/player.cpp
+++ src/player.cpp
@@ -12,12 +12,18 @@ Player::Player()
 
 void Player::Update()
 {
+	Vector right = forward.Rotate(2, 90);
+	right.y = 0;
+	right = right.Normal();
 	// TODO: Custom Controls (inputMan properties?)
 	if (inputMan->keyboardState['W'])
 		pos = pos + (forward * 0.2);
 	else if(inputMan->keyboardState['S'])
 		pos = pos + (forward * -0.2);
-	// TODO: right/left
+	if (inputMan->keyboardState['A'])
+		pos = pos + (right * -0.2);
+	else if(inputMan->keyboardState['D'])
+		pos = pos + (right * 0.2);
 	
 	MouseInput(inputMan->mouseMovement.x, inputMan->mouseMovement.y);
 
@@ -28,8 +34,8 @@ void Player::Update()
 void Player::MouseInput(float xrel, float yrel)
 {
 	// TODO: sensitivity
-	pitch += yrel * 0.05;
-	yaw += xrel * 0.05;
+	pitch += yrel * 0.1;
+	yaw += xrel * 0.1;
 
 	while (yaw > 360)
 		yaw -= 360;
