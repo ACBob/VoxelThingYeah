39b8bf395647b483a0bc40ae7edc924119ce8dbf
ACBob <acbobcat@protonmail.com>
2021-11-18 15:24:39 +0000

    number colours for my sake, add a garbage splash

--- src/shared/colours.hpp
+++ src/shared/colours.hpp
@@ -7,20 +7,20 @@
 
 // 16 colours for base dyes
 static const uint16_t DyePalette[]{
-	0x000F, // Black
-	0x555F, // Dark Grey
-	0xAAAF, // Grey
-	0xFFFF, // White
-	0x00AF, // Blue
-	0x55FF, // Light Blue
-	0x0A0F, // Green
-	0x5F5F, // Lime Green
-	0x0AAF, // Cyan
-	0x5FFF, // Turqoise [Translate to Aqua Marine in the danish localisation >:)]
-	0xA00F, // Red
-	0xF55F, // Pink
-	0xA0AF, // Purple
-	0xF5FF, // Magenta
-	0xA50F, // Brown
-	0xFF5F	// Yellow
+	0x000F, // 0 Black
+	0x555F, // 1 Dark Grey
+	0xAAAF, // 2 Grey
+	0xFFFF, // 3 White
+	0x00AF, // 4 Blue
+	0x55FF, // 5 Light Blue
+	0x0A0F, // 6 Green
+	0x5F5F, // 7 Lime Green
+	0x0AAF, // 8 Cyan
+	0x5FFF, // 9 Turqoise [Translate to Aqua Marine in the danish localisation >:)]
+	0xA00F, // 10 Red
+	0xF55F, // 11 Pink
+	0xA0AF, // 12 Purple
+	0xF5FF, // 13 Magenta
+	0xA50F, // 14 Brown
+	0xFF5F	// 15 Yellow
 };
